7dc5c1653f82b041b174ab68ebfbd41a959057db
Miha Gradišar <gradisar.mih4@gmail.com>
2025-04-16 00:11:35 +0200

    App update

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 
-import { ref } from 'vue';
-
-const isOpen = ref<boolean>(true)
-
-const drawerToggle = () => {
-  isOpen.value != isOpen.value
-}
-
 </script>
 
 <template>
-  <div class="w-0 sm:w-[250px] h-screen rounded-tr-lg rounded-br-lg overflow-hidden transition-all duration-300" :class="[isOpen ? 'bg-amber-100' : 'bg-amber-500']">
-    <h1>TEST</h1>
-    <h1>TEST</h1>
-    <h1>TEST</h1>
+  <div class="h-screen bg-blue-400 rounded-tr-lg rounded-br-lg">
+    <div @click="console.log('click!')" class="flex justify-end select-none sm:opacity-0 pointer-none">X</div>
+    <h1>Drawer</h1>
   </div>
 </template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,23 +1,41 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import AppDrawer from '@/components/AppDrawer.vue';
 
-import { ref } from 'vue';
+const isOpen = ref(false);
 
-const isOpen = ref<boolean>(false)
-
+// Drawer toggle logic
 const drawerToggle = () => {
-  isOpen.value != isOpen.value
-}
-
+  isOpen.value = !isOpen.value;
+};
 </script>
 
 <template>
-  <div class="flex">
-    <div class="">
-      <AppDrawer/>
+  <div class="flex relative">
+    <!-- Drawer -->
+    <div
+      :class="[
+        // Always visible on sm and up
+        'sm:block sm:relative sm:translate-x-0 sm:w-[250px]',
+        // Drawer on small/mobile screens
+        'transition-transform duration-300 ease-in-out',
+        'absolute max-sm:top-0 max-sm:left-0 max-sm:w-[250px] h-screen z-50',
+        isOpen ? 'translate-x-0' : '-translate-x-full',
+      ]"
+    >
+      <AppDrawer />
     </div>
-    <div class="flex">
-      TEST
+
+    <!-- Toggle button -->
+    <button
+      @click="drawerToggle"
+      :class="['h-[30px]', 'w-[30px]', 'bg-blue-900', 'sm:hidden', 'z-50', isOpen ? 'hidden' : '']"
+    >
+    </button>
+
+    <!-- Main content -->
+    <div class="flex-1">
+      Main Content
     </div>
   </div>
 </template>
