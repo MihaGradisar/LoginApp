b757e2b4a6350c1e224a38596feddadebeb74a05
Miha Gradišar <gradisar.mih4@gmail.com>
2025-04-18 01:27:41 +0200

    Added 404 page

---
+++ src/components/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center items-center h-11 w-25 rounded-full bg-indigo-500 text-white select-none cursor-pointer hover:bg-indigo-400">
+    <slot></slot>
+  </div>
+</template>
--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,6 +1,32 @@
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goBack = () => {
+  router.push({ name: 'Dashboard' })
+}
+</script>
+
 <template>
-  <div class="flex flex-col justify-center items-center h-screen w-screen max-sm:justify-start">
-    <h1>Oops!</h1>
-    <h1>404 Page Not Found</h1>
+  <div class="flex justify-center items-center h-screen w-screen">
+    <div class="w-[700px] max-sm:hidden">
+      <img src="../assets/images/404.png" alt="404">
+    </div>
+    <div class="flex flex-col justify-center items-start mb-[50px] max-sm:items-center">
+      <div class="text-[100px] font-extrabold max-sm:text-[60px]">Oops,</div>
+      <div class="text-[70px] font-extrabold max-sm:text-[40px]"><span class="text-indigo-500">Nothing</span> here...</div>
+      <div class="w-[250px] sm:hidden">
+        <img src="../assets/images/404.png" alt="404">
+      </div>
+      <p class="text-gray-400 text-left max-sm:text-center max-sm:m-[20px]">
+        Uh oh, we can't seem to find the page you're looking for.<br>
+        Try going back to previous or Contact us for for more information.
+      </p>
+      <AppButton @click="goBack" class="mt-[20px]">
+        Go Back
+      </AppButton>
+    </div>
   </div>
 </template>
